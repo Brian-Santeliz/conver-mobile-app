14bfc5152ad1dc4d3cbcc5c26e675ca31c101b8b
Brian <blsvsanteliz@gmail.com>
2021-06-05 12:59:48 -0400

    Create folder components

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  Text, View } from 'react-native';
+
+const Main = () => {
+    return (
+        <View>
+            <Text>MSIN.JS</Text>
+        </View>
+    )
+}
+
+export default Main
